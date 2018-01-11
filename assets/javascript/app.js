e7edf009879d5024ed1bfc05552c0ae0831c4d08
Wolfknightx <Wolfknightx@Gmail.com>
2018-01-11 10:07:49 -0600

    Updated system with basics

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var database = firebase.database();
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCQR9CpnVBeKM5ltwx4OLmhDWcjCmO07ik",
+    authDomain: "trainschedulerfb.firebaseapp.com",
+    databaseURL: "https://trainschedulerfb.firebaseio.com",
+    projectId: "trainschedulerfb",
+    storageBucket: "trainschedulerfb.appspot.com",
+    messagingSenderId: "1048519792785"
+};
+firebase.initializeApp(config);
