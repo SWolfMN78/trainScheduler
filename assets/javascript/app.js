091632618bfac3e718d66efc185b43b77913a98a
Wolfknightx <Wolfknightx@Gmail.com>
2018-01-15 17:19:29 -0600

    updated project need to deploy with Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,7 +20,7 @@ $("#iSubmitBtn").on("click", function(event) {
     var trainName = $("#iTrainName").val().trim();
     var trainDestin = $("#iDestin").val().trim();
     //format the below variable with moment.js
-    var trainTime = moment($("#iTrainTime").val().trim(), ).format("LTS");
+    var trainTime = moment($("#iTrainTime").val().trim(), "HH:mm").format();
     //this will be used to alter information in the moment.js later.
     var trainFreq = $("#iFreq").val().trim();
 
@@ -50,4 +50,38 @@ $("#iSubmitBtn").on("click", function(event) {
     $("#iDestin").val("");
     $("#iTrainTime").val("");
     $("#iFreq").val("");
+});
+
+//pull the information out of firebase and apply it to the app.
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+    console.log(childSnapshot.val());
+
+    //get the information from the snapshot and stick it in a var.
+    var trnName = childSnapshot.val().name;
+    var trnDest = childSnapshot.val().destination;
+    var trnStart = childSnapshot.val().time;
+    var trnFreq = childSnapshot.val().frequency;
+    //variable to help figure out timing based on the currnet time from moment.js.
+    var now = moment();
+
+    //log out the information pulled
+    console.log(trnName);
+    console.log(trnDest);
+    console.log(trnStart);
+    console.log(trnFreq);
+
+    //format the information in the database to something readable
+    var adjTimeStart = moment(trnStart, "HH:mm").format();
+
+    //calculate the information needed for the 
+    var trnDur = moment.duration(now.diff(adjTimeStart));
+    var minDiff = Math.abs(trnDur.get("minutes"));
+    var minAway = minDiff % trnFreq;
+    var nextArri = moment(now).add(minAway + 1, "minutes").format("HH:mm");
+    console.log(nextArri);
+
+    //press the information into the table.
+    $(".cTrainTable > tbody").append("<tr><td>" + trnName + "</td><td>" + trnDest + "</td><td>" +
+        trnFreq + "</td><td>" + nextArri + "</td><td>" + minAway + "</td></tr>")
 });
